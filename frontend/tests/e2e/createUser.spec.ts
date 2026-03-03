f1f2687f851ddef7071216cffe22376d8d7a58f9
Daniel <daniel.cordeiro2@fatec.sp.gov.br>
2026-03-03 17:40:03 -0300

    🧪 test: Adiciona teste e2e que simula criacao de usuario

---
+++ frontend/tests/e2e/createUser.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect, vi} from 'vitest';
+
+test("Tenta criar um usuario com nome e senha", async () => {
+    const apiURL = "http://localhost:3000/users";
+
+    const response = await fetch(apiURL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ nome: "Tadeuzinho", senha: "0000000" })
+    })
+
+    expect(response.status).toBe(201);
+
+})
